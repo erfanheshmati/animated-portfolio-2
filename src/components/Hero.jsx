c76496cb05a44a400fbcc110820ac17c2840c377
Erfan Heshmati <heshmati74@gmail.com>
2024-04-16 13:17:46 +0330

    hero section fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,11 +7,13 @@ export default function Hero() {
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Erfan Heshmati</h1>
-                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                        <h1 className="pb-16 text-6xl text-center font-thin tracking-tight lg:mt-16 lg:text-7xl">
+                            Erfan Heshmati
+                        </h1>
+                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
                             Full Stack Developer
                         </span>
-                        <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                        <p className="my-2 max-w-xl py-6 font-light tracking-tighter lg:tracking-normal text-justify">
                             {HERO_CONTENT}
                         </p>
                     </div>
