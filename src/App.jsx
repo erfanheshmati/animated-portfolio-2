1f526fc0f80bb3c3e36f2f4843baabf3ec418f11
Erfan Heshmati <heshmati74@gmail.com>
2024-04-16 12:11:09 +0330

    hero section design

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container mx-auto px-8">
         <Navbar />
+        <Hero />
       </div>
     </div>
   )
