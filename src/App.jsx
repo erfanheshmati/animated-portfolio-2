361609bc1383f240534f18e396ee66188f6e026e
Erfan Heshmati <heshmati74@gmail.com>
2024-04-16 12:54:27 +0330

    projects section design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
         <About />
         <Technologies />
         <Experience />
+        <Projects />
       </div>
     </div>
   )
